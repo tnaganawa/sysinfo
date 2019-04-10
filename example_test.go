85d1ba901bfed0064d3fc350fe4fa2e27e6918a0
Jakub Pavlik <pavlk.jakub@gmail.com>
2019-04-10 09:40:38 +0200

    Remove kernel.go to run on MacOS

--- example_test.go
+++ example_test.go
@@ -9,7 +9,7 @@ import (
 	"fmt"
 	"log"
 
-	"github.com/zcalusic/sysinfo"
+	"github.com/pupapaik/sysinfo"
 )
 
 func Example() {
--- go.mod
+++ go.mod
@@ -1,1 +1,1 @@
-module github.com/zcalusic/sysinfo
+module github.com/pupapaik/sysinfo
